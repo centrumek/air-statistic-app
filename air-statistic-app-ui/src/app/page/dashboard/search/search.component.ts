f5105b6efa2fc97a1e5867a13583a361be8545be
Marcin Pancerz <marpancerz@gmail.com>
2021-11-26 01:20:15 +0100

    [UI] search form update

--- air-statistic-app-ui/src/app/page/dashboard/search/search.component.ts
+++ air-statistic-app-ui/src/app/page/dashboard/search/search.component.ts
@@ -18,21 +18,52 @@ export class SearchComponent extends Form {
         stationNumber: {
           value: '4127',
         },
-        station: {
-          value: 'station_name',
+        stationName: {
+          value: 'Kraków Południe',
           validators: [Validators.required],
         },
-        status: {
-          value: 'status_value',
+        stationCode: {
+          value: 'KR102',
           validators: [Validators.required],
-        }
+        },
+        stationType: {
+          value: 'Lucky',
+        },
+        internationalCode: {
+          value: 'PL-KR-102',
+        },
+        voivodeship: {
+          value: 'Małopolskie'
+        },
+        address: {
+          value: 'Polna 12'
+        },
+        location: {
+          value: 'Kraków'
+        },
+        locationType: {
+          value: 'A19'
+        },
+        indicator: {
+          value: 'Benzen'
+        },
+        measurementType: {
+          value: 'Automatyczny'
+        },
+        measurementStartDate: {
+          value: '2021-02-02'
+        },
+        measurementEndDate: {
+          value: '2021-02-03'
+        },
+
       },
       validationMessages: {
-        'station': [
-          {type: 'required', message: 'Station is required'},
+        'stationName': [
+          {type: 'required', message: 'Station name is required'},
         ],
-        'status': [
-          {type: 'required', message: 'Status is required'}
+        'stationCode': [
+          {type: 'required', message: 'Station code is required'}
         ]
       }
     })
@@ -43,6 +74,7 @@ export class SearchComponent extends Form {
     if (this.form.valid) {
       this.processing = true;
       setTimeout(() => {
+        console.log(this.form.value);
         this.apiService.search(this.form.value)
           .subscribe(response => {
             // TODO search result action
