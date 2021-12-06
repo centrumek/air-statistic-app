d14fcc46fa862a6e5fc1048ce4b18b4fa40bccee
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-12-06 23:18:12 +0100

    Add Input and update navigation methods

--- air-statistic-app-ui/src/app/page/detail/detail-navigation/detail-navigation.component.ts
+++ air-statistic-app-ui/src/app/page/detail/detail-navigation/detail-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -8,18 +8,21 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DetailNavigationComponent implements OnInit {
 
+  @Input() standCode: string | "";
+
   constructor(private router: Router,
               private route: ActivatedRoute) {
+                this.standCode = this.route.snapshot.params['standCode'];
   }
 
   ngOnInit(): void {
   }
 
-  public navigateToDiagramDetailPage(): void {
-    this.router.navigate(['../diagram'], {relativeTo: this.route});
+  public navigateToDiagramDetailPage(parametr: string): void {
+    this.router.navigate(['../../diagram', parametr], {relativeTo: this.route});
   }
 
-  public navigateToTableDetailPage(): void {
-    this.router.navigate(['../table'], {relativeTo: this.route});
+  public navigateToTableDetailPage(parametr: string): void {
+    this.router.navigate(['../../table', parametr], {relativeTo: this.route});
   }
 }
