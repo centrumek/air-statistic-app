25ad1778dc1eae24702df9df6f1ce082bf94d92e
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-12-06 23:20:29 +0100

    Add stand api call

--- air-statistic-app-ui/src/app/page/detail/detail-page.service.ts
+++ air-statistic-app-ui/src/app/page/detail/detail-page.service.ts
@@ -5,15 +5,16 @@ import { Observable } from 'rxjs/internal/Observable';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { tap } from 'rxjs/operators';
 import { ObjectUtils } from '../../utils/object.utils';
+import { StandMeasurementDto } from 'src/app/model/api/stand-measurement.dto';
 
 @Injectable()
 export class DetailPageService {
 
+
   private measurements = new BehaviorSubject<StationMeasurementDto[]>([]);
+  private standMeasurements = new BehaviorSubject<StandMeasurementDto | null>(null);
 
-  constructor(private apiService: ApiService) {
-
-  }
+  constructor(private apiService: ApiService) {}
 
   public getMeasurements(stationCode: string): Observable<StationMeasurementDto[]> {
     if (ObjectUtils.tableNotNullAndNotEmpty(this.measurements.getValue())) {
@@ -22,6 +23,15 @@ export class DetailPageService {
 
     return this.apiService.getStationDetailMeasurements(stationCode)
       .pipe(tap(response => this.measurements.next(response)));
+  }
+
+  public getStandMeasurements(standCode: string): Observable<StandMeasurementDto | null> {
+    if (ObjectUtils.nonNullOrEmpty(this.standMeasurements.getValue())) {
+      return this.standMeasurements.asObservable();
+    }
+
+    return this.apiService.getStandMeasurements(standCode)
+      .pipe(tap(response => this.standMeasurements.next(response)));
   }
 
 }
