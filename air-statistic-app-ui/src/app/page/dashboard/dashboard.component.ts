bec469b2a74b09c12a4f23a037d25628287cb9d9
Marcin Pancerz <marpancerz@gmail.com>
2021-12-07 21:52:08 +0100

    [UI] search button

--- air-statistic-app-ui/src/app/page/dashboard/dashboard.component.ts
+++ air-statistic-app-ui/src/app/page/dashboard/dashboard.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-  constructor() {
+export class DashboardComponent implements AfterViewInit {
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
+
+  public ngAfterViewInit(): void {
+    if (this.router.url.includes('#search')) {
+      setTimeout(() => {
+        document.getElementById('search')?.scrollIntoView();
+      }, 100);
+    }
   }
 }
