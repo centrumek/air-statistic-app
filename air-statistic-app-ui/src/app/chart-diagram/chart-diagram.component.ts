40cb0fb5c3343d7b08032b2c70f9ee6579653462
Dominik <jurkowski.dominik.andrzej@gmail.com>
2021-12-07 21:37:06 +0100

    Add chart in diagram page

---
+++ air-statistic-app-ui/src/app/chart-diagram/chart-diagram.component.ts
@@ -0,0 +1,149 @@
+import { Component, Input, ViewChild } from "@angular/core";
+import { StandMeasurement } from '../model/stand-measurements';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexFill,
+  ApexMarkers,
+  ApexStroke
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  yaxis: ApexYAxis;
+  fill: ApexFill;
+  stroke: ApexStroke;
+  markers: ApexMarkers;
+  colors: string[];
+};
+
+@Component({
+  selector: 'app-chart-diagram',
+  templateUrl: './chart-diagram.component.html',
+  styleUrls: ['./chart-diagram.component.scss']
+})
+export class ChartDiagramComponent {
+  @ViewChild("chartdiagram") chart?: ChartComponent;
+  public chartOptions1!: Partial<ChartOptions> | any;
+  public chartOptions2!: Partial<ChartOptions> | any;
+  @Input() measurements?: StandMeasurement | null;
+
+  constructor() { 
+    
+    this.chartOptions1 = {
+      series: [
+        {
+          name: "series1",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            185,
+            {
+              min: 30,
+              max: 90
+            }
+          )
+        }
+      ],
+      chart: {
+        id: "chart2",
+        type: "line",
+        height: 230,
+        toolbar: {
+          autoSelected: "pan",
+          show: false
+        }
+      },
+      colors: ["#546E7A"],
+      stroke: {
+        width: 3
+      },
+      dataLabels: {
+        enabled: false
+      },
+      fill: {
+        opacity: 1
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
+
+    this.chartOptions2 = {
+      series: [
+        {
+          name: "series1",
+          data: this.generateDayWiseTimeSeries(
+            new Date("11 Feb 2017").getTime(),
+            185,
+            {
+              min: 30,
+              max: 90
+            }
+          )
+        }
+      ],
+      chart: {
+        id: "chart1",
+        height: 130,
+        type: "area",
+        brush: {
+          target: "chart2",
+          enabled: true
+        },
+        selection: {
+          enabled: true,
+          xaxis: {
+          }
+        }
+      },
+      colors: ["#008FFB"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          opacityFrom: 0.91,
+          opacityTo: 0.1
+        }
+      },
+      xaxis: {
+        type: "datetime",
+        tooltip: {
+          enabled: false
+        }
+      },
+      yaxis: {
+        tickAmount: 2
+      }
+    };
+  }
+
+  public generateDayWiseTimeSeries(baseval: any, count: any, yrange: any) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = baseval;
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push([x, y]);
+      baseval += 86400000;
+      i++;
+    }
+    return series;
+  }
+
+  public ngOnInit(): void {
+    console.log(this.measurements);
+  }
+
+
+}
